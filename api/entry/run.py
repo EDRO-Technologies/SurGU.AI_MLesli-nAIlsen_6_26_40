295e499fa3305dcfb77d054c69ff2da85b3586ff
niktar411 <niktar411@yandex.ru>
2025-11-08 17:18:27 +0500

    tgbot

---
+++ api/entry/run.py
@@ -0,0 +1,9 @@
+from fastapi import FastAPI
+from database import model as dbs
+import asyncio
+app = FastAPI(title="Образоательная платформа")
+
+async def create_tables():
+    await dbs.Users.create_table()
+if __name__ == '__main__':
+   asyncio.run(create_tables())
