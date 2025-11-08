ea200ca915e21f0fbb6924205883ee1f545d4df1
qrustik <guest@ca-f7.surgut.21-school.ru>
2025-11-08 19:02:38 +0500

    oi

--- bot/keyboards/menu.py
+++ bot/keyboards/menu.py
@@ -5,10 +5,10 @@ from bot.enums import consts
 
 def get_keyboard():
     keyboard = [
-        [InlineKeyboardButton(text=consts.PROFILE, callback_data="profile"),
-         InlineKeyboardButton(text=consts.TEST, callback_data="test")],
-         [InlineKeyboardButton(text="Добавить CSV историю ответов студента", callback_data="csv-document"),
-          InlineKeyboardButton(text="Добавить файлы от преподавателя")]
+        [InlineKeyboardButton(text="Профиль", callback_data="profile")],
+        [InlineKeyboardButton(text="Пройти тест", callback_data="test")],
+        [InlineKeyboardButton(text="Добавить CSV историю ответов студента", callback_data="csv_document")],
+        [InlineKeyboardButton(text="Добавить файлы от преподавателя", callback_data="asdasdsa")]
     ]
     markup = InlineKeyboardMarkup(inline_keyboard=keyboard)
     return markup
